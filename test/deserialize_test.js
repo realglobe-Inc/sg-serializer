222655c8085a63ce4f18a1173c29bf17198ac241
Taka Okunishi <okunishitaka.com@gmail.com>
2016-08-16 18:04:57 +0900

    Version incremented to 1.0.1

---
+++ test/deserialize_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for deserialize.
+ * Runs with mocha.
+ */
+'use strict'
+
+const deserialize = require('../lib/deserialize.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('deserialize', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Deserialize', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
