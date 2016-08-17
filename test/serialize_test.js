0b23c917e2b91767ae43b481ac88710830bc153e
Taka Okunishi <okunishitaka.com@gmail.com>
2016-08-17 16:05:58 +0900

    Add native objects

--- test/serialize_test.js
+++ test/serialize_test.js
@@ -27,21 +27,36 @@ describe('serialize', function () {
         date02: new Date('1985/08/26'),
         date03: [ new Date('1985/08/27'), new Date('1985/08/28') ]
       },
-      error01: new Error()
+      error01: new Error(),
+      regex01: /^hoge$/g,
+      regex02: new RegExp('^hoge$', 'g'),
+      nan01: 100 / 'hoge',
+      nan02: NaN,
+      symbol01: Symbol('hoge'),
+      symbol02: Symbol('hoge')
     }, {
       indent: 2
     })
     assert.ok(serialized)
-    // console.log(serialized)
+    console.log(serialized)
     assert.equal(typeof serialized, 'string')
 
-    let deserialized = deserialize(serialized)
-    assert.ok(deserialized)
-    assert.equal(typeof deserialized, 'object')
-    assert.ok(!deserialized.$$types)
-    assert.ok(deserialized.date01 instanceof Date)
-    assert.equal(deserialized.nested.date02 - new Date('1985/08/26'), 0)
-    assert.equal(deserialized.nested.date03[ 0 ] - new Date('1985/08/27'), 0)
+    let d = deserialize(serialized)
+    assert.ok(d)
+    assert.equal(typeof d, 'object')
+    assert.ok(!d.$$types)
+    assert.ok(d.date01 instanceof Date)
+    assert.equal(d.nested.date02 - new Date('1985/08/26'), 0)
+    assert.equal(d.nested.date03[ 0 ] - new Date('1985/08/27'), 0)
+    assert.ok(d.regex01 instanceof RegExp)
+    assert.ok(d.regex02 instanceof RegExp)
+
+    assert.ok(d.regex01.test('hoge'))
+    assert.ok(!d.regex01.test('not_hoge'))
+    assert.ok(d.regex02.test('hoge'))
+    assert.ok(!d.regex02.test('not_hoge'))
+
+    assert.equal(String(d.symbol01), 'Symbol(hoge)')
   }))
 })
 
